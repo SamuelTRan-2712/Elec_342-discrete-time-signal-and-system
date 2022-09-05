7e04f9dd3a4bdac40aa48a3e32b74e46ac992960
Samuel Tran <77118149+SamuelTRan-2712@users.noreply.github.com>
2022-09-05 16:33:21 -0400

    Create Q1_part1.m

---
+++ lab4/q1/Q1_part1.m
@@ -0,0 +1,39 @@
+periods = input('enter the number of periods: ');
+step_size = input('enter the step size that wish to use: ');
+
+global omega;
+omega = [-(periods)*pi: step_size: (periods)*pi];
+
+for index = 1:5;
+    sampling_rate = input('please enter the sampling rate: ');
+    N = floor(2* sampling_rate);
+    k = [0: 1: 2*N];
+    x = sin(2 * pi / N * k);
+    subplot(2,1,1);
+    stem (k,x);
+    xlabel('k');
+    ylabel('x[k]');
+    title ('discrete signal x[k]');
+    my_fft = fft(x,k);
+    subplot (2,1,2);
+    plot(omega, abs(my_fft));
+    xlabel('W');
+    ylabel('X(W)');
+    title ('DTFT of x[k]');
+end;    
+    
+
+%sampling function
+function fourier = fft(x,k)
+global omega;
+
+for index = 1: length(omega)
+    sum = 0;
+    for p = 1: length(x)
+        sum = sum + x(p).* exp(-1*j.* omega(index).* p);
+    end;
+    output(index) = sum;
+end;     
+    
+fourier = output;
+end
